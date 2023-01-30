ff19e6ced83257277684cad05aba4ebbd507e01c
Justas Lapiene <Justas63@gmail.com>
2023-01-30 15:23:52 +0100

    added a calculator object and its tests

--- test.js
+++ test.js
@@ -10,7 +10,22 @@
 //     expect(capitalise('string')).toBe('String')
 // })
 
-const reverse = require('./reverse')
-test('reverses a string', () => {
-    expect(reverse('pokemon')).toBe('nomekop')
+// const reverse = require('./reverse')
+// test('reverses a string', () => {
+//     expect(reverse('car')).toBe('rac')
+// })
+
+const calculator = require ('./calculator')
+
+test('calcs the correct answer', () => {
+    expect(calculator.add(2,2)).toBe(4)
+})
+test('calcs the correct answer', () => {
+    expect(calculator.subtract(2,2)).toBe(0)
+})
+test('calcs the correct answer', () => {
+    expect(calculator.multiply(5,5)).toBe(25)
+})
+test('calcs the correct answer', () => {
+    expect(calculator.divide(10, 5)).toBe(2)
 })
